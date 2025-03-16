f201ed14b19f3862c3d457b38ae9fd5b4baeb18f
istancin <stancin.igor@gmail.com>
2025-03-16 08:11:54 +0100

    New problems

--- Desktop/Problem/problem.js
+++ Desktop/Problem/problem.js
@@ -210,3 +210,285 @@ let z = 0;
 console.log(y[z] < y[z+z]);
 
 /* E sada vrijedi objasnjenje iz zadatka 39. sto cini ovu tvrdnju false. + */
+
+// Problem 41
+let y = [10, 30, 50];
+let z = 1;
+console.log(y[z - 1] + y[z + 1]);
+
+// Problem 42
+let y = [100, 200, 300];
+let z = 2;
+console.log(y[z - 1] * y[z - 2]);
+
+// Problem 43
+let y = [5, 10, 15];
+let z = 1;
+console.log(y[z] + y[z + 1] * y[z - 1]);
+
+// Problem 44
+let y = [9, 18, 27];
+let z = 1;
+console.log(y[z] * (y[z - 1] + y[z + 1]));
+
+// Problem 45
+let employee = {id: 123, name: "Steve", position: "Manager"};
+let position = "position";
+if (employee.position == "Manager") {
+  console.log("Yes");
+} else {
+  console.log("No");
+}
+
+// Problem 46
+let product = {name: "Laptop", price: 1500, stock: 30};
+let discount = 200;
+console.log(product.price - discount);
+
+// Problem 47
+let book = {title: "JavaScript Basics", author: "John Doe", pages: 200};
+let pages = "pages";
+console.log(book[pages] + 50);
+
+// Problem 48
+let user = {username: "Alice123", password: "password456", email: "alice@example.com"};
+let key = "email";
+console.log(user[key].toUpperCase());
+
+// Problem 49
+let car = {make: "Honda", model: "Civic", year: 2021};
+let carDetails = "model";
+console.log(car[carDetails] + " " + car.year);
+
+// Problem 50
+let animal = {species: "Dog", age: 5, color: "Brown"};
+let newAge = animal.age + 1;
+console.log(newAge);
+
+// Problem 51
+let students = [
+    {name: "Alice", grade: 90},
+    {name: "Bob", grade: 85},
+    {name: "Charlie", grade: 88}
+  ];
+  let index = 1;
+  console.log(students[index].name + " has grade " + students[index].grade);
+  
+  // Problem 52
+  let products = [
+    {name: "Laptop", price: 1000},
+    {name: "Phone", price: 500},
+    {name: "Tablet", price: 700}
+  ];
+  let discount = 100;
+  console.log(products[2].name + " price after discount: " + (products[2].price - discount));
+  
+  // Problem 53
+  let users = [
+    {username: "john_doe", email: "john@example.com"},
+    {username: "jane_doe", email: "jane@example.com"}
+  ];
+  let userIndex = 0;
+  console.log(users[userIndex].username + " - " + users[userIndex].email);
+  
+  // Problem 54
+  let cars = [
+    {make: "Toyota", model: "Camry", year: 2020},
+    {make: "Honda", model: "Civic", year: 2021}
+  ];
+  let carIndex = 1;
+  console.log("Car model: " + cars[carIndex].model + ", Year: " + cars[carIndex].year);
+  
+  // Problem 55
+  let library = [
+    {title: "JavaScript for Beginners", author: "John Smith", pages: 200},
+    {title: "Advanced JavaScript", author: "Jane Doe", pages: 300}
+  ];
+  let bookIndex = 0;
+  console.log(library[bookIndex].title + " by " + library[bookIndex].author);
+  
+  // Problem 56
+  let pets = [
+    {name: "Buddy", species: "Dog", age: 3},
+    {name: "Whiskers", species: "Cat", age: 2}
+  ];
+  let petIndex = 1;
+  console.log(pets[petIndex].name + " is a " + pets[petIndex].species + " aged " + pets[petIndex].age);
+
+  // Problem 57
+let x = [10, 20, 30, 40];
+let y = {a: 2, b: 5};
+console.log(x[2] * y.a - y.b);
+
+// Problem 58
+let x = [100, 200, 300];
+let y = {p: 20, q: 50};
+console.log(x[0] - y.p + x[2] - y.q);
+
+// Problem 59
+let x = [25, 30, 35];
+let y = {m: 1000, n: 500};
+console.log(x[1] + y.m - y.n);
+
+// Problem 60
+let x = [90, 80, 70];
+let y = {g1: 10, g2: 5};
+console.log(x[0] + y.g1 - y.g2);
+
+// Problem 61
+let x = [5, 10, 15, 20];
+let y = {a: 3, b: 2};
+console.log(x[1] ** y.a + y.b);
+
+// Problem 62
+let x = [10, 20, 30, 40];
+let y = {m: 5, n: 15};
+console.log((x[3] - y.m) * (x[1] / y.n));
+
+// Problem 63
+let x = [7, 14, 21, 28];
+let y = {a: 3, b: 4};
+console.log(x[2] % y.a + y.b);
+
+// Problem 64
+let x = [5, 15, 25];
+let y = {p: 2, q: 1};
+console.log(x[0] + x[1] * y.p - x[2] / y.q);
+
+// Problem 65
+let x = [10, 5, 3];
+let y = {m: 8, n: 2};
+console.log((x[0] + y.m) * (x[1] - y.n) / x[2]);
+
+// Problem 66
+let x = [10, 20, 30, 40];
+let y = {a: 1, b: 2, c: 3};
+console.log(x[y.a + y.b] - x[y.c]);
+
+// Problem 67
+let x = [5, 10, 15, 20, 25];
+let y = {a: 0, b: 2, c: 1};
+console.log(x[y.a + y.b] * x[y.c + y.b]);
+
+// Problem 68
+let x = [2, 4, 6, 8, 10];
+let y = {p: 0, q: 2, r: 1};
+console.log(x[y.p + y.q] - x[y.r * y.p]);
+
+ // Problem 69
+let x = [1, 2, 3, 4, 5];
+let y = {a: 2, b: 1};
+console.log(x[x[y.a] - y.b]);
+
+ // Problem 70
+let x = [100, 200, 300, 400, 500];
+let y = {m: 1, n: 2};
+console.log(x[x[y.m + y.n]] / 2);
+
+// Problem 71
+let x = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+let y = {a: 1, b: 2};
+console.log(x[y.a][y.b]);
+
+ // Problem 72
+let x = [[10, 20, 30], [40, 50, 60], [70, 80, 90]];
+let y = {a: 0, b: 1};
+console.log(x[y.a][y.b]);
+
+ // Problem 73
+let x = [[100, 200], [300, 400], [500, 600]];
+let y = {a: 2, b: 0};
+console.log(x[y.a][y.b]);
+
+//Problem 74
+let x = [[10, 20, 30], [40, 50, 60], [70, 80, 90]];
+let y = {a: 0, b: 1};
+let z = {p: 2};
+console.log(x[y.a][y.b] * x[y.a][z.p]);
+
+ // Problem 75
+let x = [[5, 10, 15], [20, 25, 30], [35, 40, 45]];
+let y = {a: 2, b: 0};
+let z = {p: 1};
+console.log(x[y.a][y.b] + x[z.p][y.b]);
+
+ // Problem 76
+let x = [[3, 6, 9], [12, 15, 18], [21, 24, 27]];
+let y = {a: 1, b: 2};
+let z = {p: 0};
+console.log(x[y.a][y.b] - x[z.p][z.p]);
+
+// Problem 77
+let x = [[3, 6, 9], [12, 15, 18], [21, 24, 27]];
+let y = [2,1,0]
+let z = [1,2,0]
+console.log(x[y[0]][z[0]] == x[z[0]][y[0]])
+
+// Problem 78
+let x = [[3, 6, 9], [12, 15, 18], [21, 24, 27]];
+let y = [2,1,0]
+let z = [1,2,0]
+console.log(x[y[2]] == x[z[2]])
+
+// Problem 79
+let x = [[3, 6, 9], [12, 15, 18], [21, 24, 27]];
+let y = [2,1,0]
+let z = [1,2,0]
+console.log(x[z[y[1]]][z[2]] + x[z[2]][y[z[0]]])
+
+// Problem 80
+let x = [[3, 6, 9], [12, 15, 18], [21, 24, 27]];
+let y = [2,1,0]
+let z = [1,2,0]
+console.log(x[y[1]][y[1]] + x[y[z[0]]][y[1]] * x[y[1]][z[0]])
+
+// Problem 81
+let x = [[3, 6, 9], [12, 15, 18], [21, 24, 27]];
+let y = [2,1,0]
+let z = [1,2,0]
+if (y[z[0]] > z[y[0]]) {
+    console.log(x[1][0])
+} else {
+    console.log(z)
+}
+
+// Problem 82
+let x = [[3, 6, 9], [12, 15, 18], [21, 24, 27]];
+let y = [2, 1, 0];
+let z = [0, 2, 1];
+function checkCondition(array1, array2, array3) {
+    if (array1[array2[0]][array3[1]] > array1[array3[0]][array2[2]]) {
+        console.log(3+3*3);
+    } else {
+        console.log(3-3*3);
+    }
+}
+checkCondition(x, y, z);
+
+// Problem 83
+let x = [[4, 8, 12], [16, 20, 24], [28, 32, 36]];
+let y = [1, 0, 2];
+let z = [2, 1, 0];
+function evaluateCondition(array1, array2, array3) {
+    if (array1[array2[1]][array3[0]] < array1[array3[2]][array2[0]]) {
+        console.log(3+3/3+3);
+    } else {
+        console.log(3-3/3-3);
+    }
+}
+evaluateCondition(x, z, y);
+
+// Problem 84
+let x = [[4, 8, 12], [16, 20, 24], [28, 32, 36]];
+let y = [1, 0, 2];
+let z = [2, 1, 0];
+function evaluateCondition(array1, array3, array2) {
+    if (array1[array2[1]][array3[0]] < array1[array3[2]][array2[0]]) {
+        console.log("My name is Sue");
+    } else if (array1[array2[1]][array3[0]] == array1[array3[2]][array2[0]]) {
+        console.log("How do you do?");
+    } else {
+        console.log("Now you gonna die");
+    }
+}
+evaluateCondition(x, z, y);
