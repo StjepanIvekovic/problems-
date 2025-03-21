5cb20cb7133ea3a5d378d667c70c4a58f6983091
istancin <stancin.igor@gmail.com>
2025-03-21 16:22:12 +0100

    Solve this

---
+++ problem_2.js
@@ -0,0 +1,20 @@
+// Write a function that recieves two objects and returns array that contain these two objects
+
+// Write a function that recieves array and integer i. If array has i-th element write it to console 
+// and if not then write "So sad".
+
+// Write a function that recieves array of integers and writes them to console in reverse order.
+
+// Write a function that recieves object o and string s. If object contain name s, write "Feel good", 
+// otherwise write "So sad".
+
+// Write a function that recieves two numbers and if first one is larger  write to console "whatever", 
+// if second is larger write "everwhat" and if they are equal write "nevernot".
+
+// Write a function that recieves array and integer i. write i-th number of array but counting from the
+// in the reverse order (from the end of array).
+
+// Write a function that recieves object that containe name "arr1". Value for name "arr1" is array with
+// integers. Function returns sum of elements in that array which are divisible by 10.
+
+// Write a function that recieves array and returns true if array is palindrom and false if it is not.
