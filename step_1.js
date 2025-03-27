ab18c2c0bd0f729122475604bf73f15eb7e7f4d1
istancin <stancin.igor@gmail.com>
2025-03-27 21:52:32 +0100

    new problems

---
+++ step_1.js
@@ -0,0 +1,29 @@
+// Declare variables x and y in two lines of code (two statments)
+
+// Declare variables x and y in one line of code (one statement)
+
+// Declare variables x and y and then assign values 5 and 6 to these variables
+
+// Declare a variable x. Assign value 5 to variable x and then reassigne it to value 10
+
+// Declare and assign variable x whose value can not be changed
+
+// Declare and assign variable x whose datatype will be string
+
+// Create variable whose value is undefined
+
+// Assign value 5 to variable x. Then assign value of x to variable y
+
+// Use any arithmetic operators in assignement statement
+
+// Create two variables, both with value 5. You can use number 5 only once to do so.
+
+// Declare and assign two variables and them multiply them and store the result in third variable.
+
+// Write symbols of four arithmetic operators to console
+
+// Write to console example of single character that can not be on the first place of variable name
+
+// Create variables xx, XX and x-x. Assgin three different values to them and write each to console
+
+// Declare variables x, y and z. Assign value 11 to variable z without ever using number 1 in code.
