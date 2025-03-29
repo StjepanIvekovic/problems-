8348a2075a71dcf8694d8b4a24ba050559c1dbbb
Stjepan Iveković <stjepan.ivekovic@gmail.com>
2025-03-29 14:47:49 +0100

    solutions

--- step_1.js
+++ step_1.js
@@ -1,29 +1,69 @@
 // Declare variables x and y in two lines of code (two statments)
+let x;
+let y;
+console.log(x,y);
 
 // Declare variables x and y in one line of code (one statement)
-
+let x, y;
+console.log(x,y);
 // Declare variables x and y and then assign values 5 and 6 to these variables
+const x = 5; 
+const y = 6;
+console.log(x,y);  
 
 // Declare a variable x. Assign value 5 to variable x and then reassigne it to value 10
+let x = 5; 
+x = 10; 
+console.log(x); 
 
 // Declare and assign variable x whose value can not be changed
+const x = 10; 
+console.log(x); 
 
 // Declare and assign variable x whose datatype will be string
+let x = ""; 
+console.log(x); 
 
 // Create variable whose value is undefined
+let x; 
+console.log(x); 
 
 // Assign value 5 to variable x. Then assign value of x to variable y
+let x = 5; 
+let y = x; 
+console.log(y); 
 
 // Use any arithmetic operators in assignement statement
+let x = (20 + 5) - (10 + 5) / 2; 
+console.log(x); 
+
 
 // Create two variables, both with value 5. You can use number 5 only once to do so.
+let x = 5; 
+let y = x; 
+console.log(x,y); 
 
 // Declare and assign two variables and them multiply them and store the result in third variable.
+let x = 10; 
+let y = 20; 
+let z = 10 * 20; 
+console.log(z); 
 
 // Write symbols of four arithmetic operators to console
++, -, /,*
 
 // Write to console example of single character that can not be on the first place of variable name
+let 3 = 0; 
+console.log(3); // the variable cannot begin with a number 
 
 // Create variables xx, XX and x-x. Assgin three different values to them and write each to console
+let xx = ["Juice", "Goose", "Moose"]; 
+let XX = "Bravo"; 
+let x3x = "Wow";   
+console.log(xx, XX, x3x); // We cant have x-x variable beacuse it is not supported in naming the variable
 
 // Declare variables x, y and z. Assign value 11 to variable z without ever using number 1 in code.
+let x; 
+let y;
+let z = "11"; 
+console.log(z); 
