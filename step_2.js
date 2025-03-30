ec85c66023be24270a2399173149746d4e77e0cf
istancin <stancin.igor@gmail.com>
2025-03-30 09:46:58 +0200

    New tasks

---
+++ step_2.js
@@ -0,0 +1,32 @@
+// Assign two variables to values 5 and 6. Write code logic that will swap values in 
+// these two variables. (without using number 5 and 6 again)
+
+// Assign a variable to a value of 12. Write code that will multiply the value of this variable by 5
+// and store the result back in the same variable.
+
+// Use two variables. Write code that three times checks if these two variables are equal.
+// Results of these checks must be in this order - true, false, true. Each variable can be 
+// assigned at most 2 times.
+
+// U Const poglavlju pise ovo "It does not define a constant value. It defines a constant 
+// reference to a value.". Jel razumijs kaj to znaci? Kaj je referenca, a kaj je value?
+
+// Assign one variable as "37" and other as "7". both string. Compare them with > or < and 
+// write it with console log.
+
+// Assigne three variables to 0, 10 and undefined. On all three apply operator that check if 
+// variable is undefined if it is value is assgned.
+
+// Declare a variable x outside of a block and assign it a value of 12. 
+// Also declare a global variable y=20. Inside the block, declare another variable x with
+// a value of 5. In that block add new block and assign x to 3. In that block create new
+// block and assign x to 3 and y to 50. Print (console.log) both variables inside each scope that exists. 
+// Try to understand the output.
+
+// Declare and assigne variables x and y. Izračunaj potenciju x na y i spremi u varijablu z.
+// Do it all with just 2 lines of code.
+
+// Assign a variable `a` to 15, `b` to undefined, and `c` to 25. 
+// Write code that checks if `b` is undefined. If so, assign it the value of `a`.
+// Then, write a check to see if `c` is undefined, and if so, assign it the value of `b`.
+// Print all the values at the end.
